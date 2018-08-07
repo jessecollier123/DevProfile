6801972b6958e32629bbf70b52ee8011ae699a19
Jesse Collier <jessecollier90@hotmail.com>
2018-08-06 20:57:46 -0400

    Added Tablet Layout and Constraints for Phones

--- app/src/main/java/com/example/jesse1/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/jesse1/devprofile/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.jesse1.devprofile
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //round the Devslopes logo
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        //or comment the next line and set rounded.isCircular = true to have a round circle
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
     }
 }
